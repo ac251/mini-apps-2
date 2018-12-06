b4351940fbbc44dec63ab5bd95b28eabae7fcdda
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-12-05 16:12:38 -0800

    updated config

---
+++ challenge_1/.eslintrc.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+ "extends": "airbnb",
+ "parser": "babel-eslint",
+ "plugins": ["react", "jsx-a11y", "import"],
+ env: {
+  browser: true,
+  node: true,
+  es6: true,
+ },
+ rules: {
+  'react/prop-types': 0,
+ }
+}
+
--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -6,12 +6,15 @@ module.exports = {
     filename: 'app.js',
     path: path.join(__dirname, 'public'),
   },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: 'babel-loader'
-      }
-    ]
-  }
+        use: 'babel-loader',
+      },
+    ],
+  },
 };
