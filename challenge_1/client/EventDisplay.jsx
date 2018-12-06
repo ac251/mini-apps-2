63158a13eeb315135caaac36059501bee7e613ea
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-12-05 16:14:13 -0800

    wrote first basic version of component to display each event.

--- challenge_1/client/EventDisplay.jsx
+++ challenge_1/client/EventDisplay.jsx
@@ -1,1 +1,18 @@
 import React from 'react';
+import Moment from 'moment';
+
+const EventDisplay = ({ event }) => {
+  const { date, description } = event;
+  return (
+    <div>
+      <div>
+        {Moment(date)}
+      </div>
+      <div>
+        {description}
+      </div>
+    </div>
+  );
+};
+
+export default EventDisplay;
