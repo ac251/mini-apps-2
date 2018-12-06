d99ba1bc82179cd3627b990684f067e26b2ab7be
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-12-06 14:43:26 -0800

    Implemented api route through server to get data from coindesk

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const path = require('path');
+const request = require('request');
 
+const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/prices', (req, res) => {
+  const { start, end } = req.query;
+  const queries = start && end ? { start, end } : undefined;
+  request('https://api.coindesk.com/v1/bpi/historical/close.json',{
+    method: 'GET',
+    qs: queries
+  })
+  .pipe(res);
+});
+
+app.listen(3000);
 
