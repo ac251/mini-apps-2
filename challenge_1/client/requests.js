f9380ed300108c7612161fcd15b6f1bfe084fb80
Aaron Cohn <aaron.d.cohn@gmail.com>
2018-12-05 19:28:53 -0800

    added method to send PATCH request

--- challenge_1/client/requests.js
+++ challenge_1/client/requests.js
@@ -15,4 +15,12 @@ export default {
       })
       .catch(err => console.log(err));
   },
+
+  updateEntry(id, data) {
+    return axios.patch(`/events/${id}`, {
+      data,
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .catch(err => console.log(err));
+  },
 };
